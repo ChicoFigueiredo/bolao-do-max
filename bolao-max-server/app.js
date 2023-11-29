1decec0590253e7da20e5b0877b46ee1f4bf6a0b
Francisco Figueiredo <fran.fig@gmail.com>
2023-11-29 07:54:13 -0300

    Chico: duration

--- bolao-max-server/app.js
+++ bolao-max-server/app.js
@@ -11,6 +11,31 @@ const atualiza_redis = require('./atualiza-redis');
 
 var app = express();
 
+const getDurationInMilliseconds = (start) => {
+    const NS_PER_SEC = 1e9
+    const NS_TO_MS = 1e6
+    const diff = process.hrtime(start)
+
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+}
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl} [STARTED]`)
+    const start = process.hrtime()
+
+    res.on('finish', () => {            
+        const durationInMilliseconds = getDurationInMilliseconds (start)
+        console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+
+    res.on('close', () => {
+        const durationInMilliseconds = getDurationInMilliseconds (start)
+        console.log(`${req.method} ${req.originalUrl} [CLOSED] ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+
+    next()
+})
+
 (async () => {
  
     app.client_redis = redis.createClient({
--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -1,4 +1,5 @@
 git pull origin master
 git reset --hard origin master
 docker compose down
-docker compose up -d --build --force-recreate
+docker system prune
+docker compose up -d --build --force-recreate --always-recreate-deps
